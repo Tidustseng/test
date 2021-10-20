97408bde6f12d1dff14bacf0298bbf1e102fc521
Tidustseng <31063431+Tidustseng@users.noreply.github.com>
2021-10-20 10:54:09 +0800

    Update socket_client.py
    add comment mark in line 7 and 9

--- socket_client.py
+++ socket_client.py
@@ -4,9 +4,9 @@ PORT = 8001
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-please add comment here
+#please add comment here
 while True:
-    cmd = raw_input("Please input msg:")
+    #cmd = raw_input("Please input msg:")
     cmd = '1 2'
     s.send(cmd)
     data = s.recv(1024)
